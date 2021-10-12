6eb7209bc6f5b8f152d80f4c8e3960d4cf113f61
vaskochobanov <vasko.chobanov@gmail.com>
2021-10-12 20:30:24 +0300

    Registration form finished

---
+++ src/main/java/app/restaurant/web/UserController.java
@@ -0,0 +1,14 @@
+package app.restaurant.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register";
+    }
+}
