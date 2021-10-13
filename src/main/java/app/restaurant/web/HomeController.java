c8e913bcb10331ec09e24248bbfc3d49df1d1425
vaskochobanov <vasko.chobanov@gmail.com>
2021-10-13 20:09:44 +0300

    Home-admin loaded

--- src/main/java/app/restaurant/web/HomeController.java
+++ src/main/java/app/restaurant/web/HomeController.java
@@ -1,12 +1,27 @@
 package app.restaurant.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String getIndex() {
         return "index";
+    }
+    @GetMapping("/home")
+    public String getHome() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<String> roles = authentication.getAuthorities().stream().map(a -> a.getAuthority()).collect(Collectors.toList());
+        String role = roles.get(0);
+        if (role.equals("ROLE_ADMIN")) {
+            return "home-admin";
+        }
+        return null;
     }
 }
