f9f50a6e58a88b97174bfe7ac9e7bc4ac6ec5951
vaskochobanov <vasko.chobanov@gmail.com>
2021-10-21 21:38:12 +0300

    Filtering ingredients done

--- src/main/java/app/restaurant/web/ApiController.java
+++ src/main/java/app/restaurant/web/ApiController.java
@@ -1,6 +1,8 @@
 package app.restaurant.web;
 
+import app.restaurant.models.dtos.IngredientViewDto;
 import app.restaurant.models.dtos.MealViewDto;
+import app.restaurant.services.IngredientService;
 import app.restaurant.services.MealService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +15,19 @@ import java.util.List;
 @RequestMapping("/api")
 public class ApiController {
     private final MealService mealService;
+    private final IngredientService ingredientService;
 
-    public ApiController(MealService mealService) {
+    public ApiController(MealService mealService, IngredientService ingredientService) {
         this.mealService = mealService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/meals")
     public ResponseEntity<List<MealViewDto>> loadAllMeals() {
         return ResponseEntity.status(200).body(mealService.getAllMealsSorted());
+    }
+    @GetMapping("/ingredients")
+    public ResponseEntity<List<IngredientViewDto>> loadAllIngredients() {
+        return ResponseEntity.status(200).body(ingredientService.getAllIngredientsSorted());
     }
 }
---
+++ src/main/resources/static/js/filter-ingredients.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let filterField = document.getElementById("filterField");
+    let tableBody = document.getElementById("tableBody");
+    let createRow = (el) => {
+      let newTR = document.createElement("tr");
+      let tdImage = document.createElement("td");
+      newTR.appendChild(tdImage);
+      let divImage = document.createElement("div");
+      tdImage.appendChild(divImage);
+      divImage.classList.add("ingredient-table-row");
+      let imgImage = document.createElement("img");
+      divImage.appendChild(imgImage);
+      imgImage.classList.add("ingredient-image");
+      imgImage.src = el.imageUrl;
+      let tdName = document.createElement("td");
+      newTR.appendChild(tdName);
+      let divName = document.createElement("div");
+      tdName.appendChild(divName);
+      divName.classList.add("ingredient-table-row");
+      divName.innerText = el.name;
+      let tdQuantity = document.createElement("td");
+      newTR.appendChild(tdQuantity);
+      let divQuantity = document.createElement("div");
+      tdQuantity.appendChild(divQuantity);
+      divQuantity.classList.add("ingredient-table-row");
+      divQuantity.innerText = el.quantity;
+      let tdBestBefore = document.createElement("td");
+      newTR.appendChild(tdBestBefore);
+      let divBestBefore = document.createElement("div");
+      tdBestBefore.appendChild(divBestBefore);
+      divBestBefore.classList.add("ingredient-table-row");
+      divBestBefore.innerText = el.bestBefore;
+      let tdButton = document.createElement("td");
+      newTR.appendChild(tdButton);
+      let divButton = document.createElement("div");
+      tdButton.appendChild(divButton);
+      divButton.classList.add("form-button");
+      let formButton = document.createElement("form");
+      divButton.appendChild(formButton);
+      formButton.method = "GET";
+      formButton.action = `/ingredients/delete/${el.id}`;
+      formButton.classList.add("form-button-delete");
+      let deleteButton = document.createElement("button");
+      formButton.appendChild(deleteButton);
+      deleteButton.type = "submit";
+      deleteButton.classList.add("btn");
+      deleteButton.classList.add("btn-danger");
+      deleteButton.innerText = "Delete";
+      tableBody.appendChild(newTR);
+    }
+    fetch("http://localhost:8080/api/ingredients", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        let strigifiedResponce = JSON.stringify(data);
+        let mealsArr = JSON.parse(strigifiedResponce);
+        let filteredArr = [...mealsArr];
+        filterField.addEventListener("input", (event) => {
+            tableBody.innerHTML = "";
+          filteredArr = mealsArr.filter((m) =>
+            m.name.toLowerCase().includes(event.target.value)
+          );
+          filteredArr.forEach(m => createRow(m));
+      });
+      filteredArr.forEach((m) => {
+        createRow(m);
+        });
+      });
+      window.addEventListener("pageshow", (event) => {
+        filterField.value = "";
+      });
+  });
