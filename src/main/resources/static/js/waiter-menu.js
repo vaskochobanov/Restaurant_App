afc80d3a2764f45e12603198b9c82389fd238990
vaskochobanov <vasko.chobanov@gmail.com>
2021-10-29 13:54:13 +0300

    Menu plus and minus buttons worked now

---
+++ src/main/resources/static/js/waiter-menu.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let filterField = document.getElementById("filterField");
+    let waiterId = document.getElementById("waiterId");
+    let tabbleId = document.getElementById("tabbleId");
+    let mainContent = document.getElementById("mainContent");
+    let createRow = (el) => {
+        let divWrappingCard = document.createElement("div");
+        mainContent.appendChild(divWrappingCard);
+        divWrappingCard.classList.add("card");
+        let divMealType = document.createElement("div");
+        divWrappingCard.appendChild(divMealType);
+        divMealType.classList.add("card-header", "waiter-menu");
+        divMealType.innerText = el.type;
+        let divCardBody = document.createElement("div");
+        divWrappingCard.appendChild(divCardBody);
+        divCardBody.classList.add("card-body", "waiter-menu");
+        let h5MealName = document.createElement("h5");
+        divCardBody.appendChild(h5MealName);
+        h5MealName.classList.add("card-title");
+        h5MealName.innerText = el.name;
+        let pMealDescription = document.createElement("p");
+        divCardBody.appendChild(pMealDescription);
+        pMealDescription.classList.add("card-text", "waiter-order-meals");
+        pMealDescription.innerText = el.description;
+        let pMealIngredients = document.createElement("p");
+        divCardBody.appendChild(pMealIngredients);
+        pMealIngredients.classList.add("card-text", "waiter-order-meals");
+        pMealIngredients.innerText = el.ingredients;
+        let divMealButtonsHolder = document.createElement("div");
+        divCardBody.appendChild(divMealButtonsHolder);
+        divMealButtonsHolder.classList.add("menu-buttons-holder");
+        let minusButton = document.createElement("button");
+        divMealButtonsHolder.appendChild(minusButton);
+        minusButton.type = "button";
+        minusButton.classList.add("btn", "btn-danger", "menu-buttons");
+        minusButton.innerText = "-";
+        minusButton.addEventListener("click", () => {
+            let currentNumber = Number(mealsCount.value);
+            currentNumber--;
+            mealsCount.value = currentNumber;
+        })
+        let mealsCount = document.createElement("input");
+        divMealButtonsHolder.appendChild(mealsCount);
+        mealsCount.type = "text";
+        mealsCount.classList.add("form-control", "menu-meal-count");
+        mealsCount.value = "0";
+        let plusButton = document.createElement("button");
+        divMealButtonsHolder.appendChild(plusButton);
+        plusButton.type = "button";
+        plusButton.classList.add("btn", "btn-success", "menu-buttons");
+        plusButton.innerText = "+";
+        plusButton.addEventListener("click", () => {
+            let currentNumber = Number(mealsCount.value);
+            currentNumber++;
+            mealsCount.value = currentNumber;
+        })
+    };
+    fetch("http://localhost:8080/api/waiter-menu", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      let strigifiedResponce = JSON.stringify(data);
+      let mealsArr = JSON.parse(strigifiedResponce);
+      let filteredArr = [...mealsArr];
+      filterField.addEventListener("input", (event) => {
+        mainContent.innerHTML = "";
+        filteredArr = mealsArr.filter((m) =>
+          m.name.toLowerCase().includes(event.target.value.toLowerCase())
+        );
+        filteredArr.forEach((m) => createRow(m));
+      });
+      filteredArr.forEach((m) => {
+        createRow(m);
+      });
+    window.addEventListener("pageshow", (event) => {
+      filterField.value = "";
+    });
+  });
+})
